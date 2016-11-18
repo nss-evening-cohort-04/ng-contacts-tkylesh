a52db5c2750d7c50d9576f2bd81bd94010036c11
Taylor Shuff <tkylesh@gmail.com>
2016-11-17 19:10:58 -0600

    added a get mathod to return the contact list from firebase db.

--- javascripts/factories/contactFactory.js
+++ javascripts/factories/contactFactory.js
@@ -1,1 +1,21 @@
 "use strict";
+
+
+app.factory("ItemFactory",function($q, $http, FIREBASE_CONFIG){
+	var getContactList = function(){
+		return $q((resolve,reject)=>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json`)
+			.success(function(response){
+				let contacts = [];
+				Object.keys(response).forEach(function(key){
+					response[key].id=key;
+					contacts.push(response[key]);
+				});
+				resolve(contacts);
+			})
+			.error(function(errorResponse){
+				reject(errorResponse);
+			});
+		});
+	};
+});
